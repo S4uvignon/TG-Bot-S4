209efc3c9a0b79f4ce3db837bc873e042c1516e7
S4uvignon <136178916+S4uvignon@users.noreply.github.com>
2026-02-05 13:57:31 +0700

    v1.2
    Groq

--- config.py
+++ config.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+# DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
 # ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
 # OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 HH_API_BASE_URL = 'https://api.hh.ru'
-DEEPSEEK_BASE_URL = 'https://api.deepseek.com'
+GROQ_BASE_URL = 'https://api.groq.com/openai/v1'
+# DEEPSEEK_BASE_URL = 'https://api.deepseek.com'
