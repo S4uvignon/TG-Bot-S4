06d93021cf75e53020e273af8906869195068ff1
S4uvignon <136178916+S4uvignon@users.noreply.github.com>
2026-02-05 19:40:07 +0700

    Add ADMIN_IDS to configuration

--- config.py
+++ config.py
@@ -12,3 +12,5 @@ GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 HH_API_BASE_URL = 'https://api.hh.ru'
 GROQ_BASE_URL = 'https://api.groq.com/openai/v1'
 # DEEPSEEK_BASE_URL = 'https://api.deepseek.com'
+
+ADMIN_IDS = [1183964350]
