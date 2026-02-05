6f26cee8cee7691f71168c331928cb5cbb8e85f3
S4uvignon <136178916+S4uvignon@users.noreply.github.com>
2026-02-05 13:22:49 +0700

    V1.1
    CloudAI заменил на DeepSeek

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
+DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
+# ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
 # OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 HH_API_BASE_URL = 'https://api.hh.ru'
+DEEPSEEK_BASE_URL = 'https://api.deepseek.com'
