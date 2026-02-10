7cbe98670f833ce157f57497030e95e22ad648b5
S4uvignon <136178916+S4uvignon@users.noreply.github.com>
2026-02-10 21:31:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY') # Ключ от Google AI Studio
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+GROQ_BASE_URL = os.getenv('GROQ_BASE_URL', 'https://api.groq.com/openai/v1')
 
 HH_API_BASE_URL = 'https://api.hh.ru'
+
 ADMIN_IDS = [1183964350]
