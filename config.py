bb33f8d362464b2f9755933a1834585495f6fbd1
S4uvignon <136178916+S4uvignon@users.noreply.github.com>
2026-02-10 15:27:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-GROQ_API_KEY = os.getenv('GROQ_API_KEY')
-# DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
-# ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
-# OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY') # Ключ от Google AI Studio
 
 HH_API_BASE_URL = 'https://api.hh.ru'
-GROQ_BASE_URL = 'https://api.groq.com/openai/v1'
-# DEEPSEEK_BASE_URL = 'https://api.deepseek.com'
-
 ADMIN_IDS = [1183964350]
